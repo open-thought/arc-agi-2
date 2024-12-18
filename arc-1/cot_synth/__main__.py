91b4411376384d273249aafd10cf9f53606ae9ed
Andreas Koepf <andreas.koepf@aleph-alpha.com>
2024-12-18 08:25:48 +0100

    add loading of remix-arc dataset

--- arc-1/cot_synth/__main__.py
+++ arc-1/cot_synth/__main__.py
@@ -1,4 +1,8 @@
 import argparse
+from pathlib import Path
+import json
+import pickle
+from tqdm import tqdm
 
 # Llama 3.2-Vision 11B, 128k context
 # native image tile size of vision adapter: 448 px
@@ -32,8 +36,52 @@ def parse_args():
     return args
 
 
+def load_remix_arc_dataset(dataset_path: Path) -> dict[str, dict]:
+    riddle_ids = [p.stem for p in dataset_path.glob("*.png")]
+
+    riddles = {}
+
+    # load riddles & their meta-data
+    for rid in tqdm(riddle_ids):
+        riddle_path = dataset_path / (rid + ".json")
+        riddle_meta_path = dataset_path / (rid + ".meta.json")
+
+        board_pairs = json.loads(riddle_path.read_text())
+        riddle_meta_data = json.loads(riddle_meta_path.read_text())
+
+        riddles[rid] = {
+            "generator_fn": riddle_meta_data["generator_fn"],
+            "verifier_fn": riddle_meta_data["verifier_fn"],
+            "thoughts_fn": riddle_meta_data["thoughts"],
+            "idea": riddle_meta_data["idea"],
+            "board_pairs": board_pairs,
+        }
+
+    return riddles
+
+
+def cache_remix_arc_dataset(dataset_path: Path, cache_path: Path) -> dict[str, dict]:
+    remix_arc_pickle_path = cache_path / "remix-arc-1.3k.pickle"
+    if remix_arc_pickle_path.exists():
+        with remix_arc_pickle_path.open("rb") as f:
+            remix_arc_data = pickle.load(f)
+    else:
+        remix_arc_data = load_remix_arc_dataset(dataset_path)
+        cache_path.mkdir(exist_ok=True)
+        with remix_arc_pickle_path.open("wb") as f:
+            pickle.dump(remix_arc_data, f)
+    return remix_arc_data
+
+
 def main():
     args = parse_args()
+
+    print("importing dataset")
+    remix_arc_dateset_path = Path("/data/remix-arc-1.3k/")
+    data_cache_path = Path(".cache")
+
+    x = cache_remix_arc_dataset(remix_arc_dateset_path, data_cache_path)
+    print(len(x))
 
 
 if __name__ == "__main__":
